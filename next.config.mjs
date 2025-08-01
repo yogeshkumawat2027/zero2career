7bd9a94bf70a2d5c3e15e38825f6fc259e1e1fe6
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2025-08-02 00:07:25 +0530

    fix: update image domains in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: [
+      'images.unsplash.com',
+      'cdn.siasat.com',
+      'aerocadet.com',
+      'iikd.in',
+      'www.dice.com',
+      'www.healthathomes.com',
+      'i0.wp.com',
+      'indiancombat.com'
+    ],
   },
 };
 
