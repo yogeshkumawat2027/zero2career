7f9bff1a5896f238c0a3d6df1298eceda9527153
yogeshkumawat2027 <yogeshkumawat2027@gmail.com>
2025-08-04 01:25:43 +0530

    feat: update image URLs in careers data and add new image domains in configuration

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,13 @@ const nextConfig = {
       'www.dice.com',
       'www.healthathomes.com',
       'i0.wp.com',
-      'indiancombat.com'
+      'indiancombat.com',
+      'www.theladders.com',
+      't4.ftcdn.net',
+      'encrypted-tbn0.gstatic.com',
+      'www.watermillaccounting.co.uk',
+      'shop.ssbcrack.com',
+      'images.hindustantimes.com'
     ],
   },
 };
